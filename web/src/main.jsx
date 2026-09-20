db567998fcba08629ae2d05fe6e6f8b9fec86e20
Alexis Brian Reyes Saavedra <65685400+deepanalytica@users.noreply.github.com>
2026-09-20 12:36:37 -0300

    perf(rooms): preload all production GLBs used after startup

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -5,10 +5,15 @@ import App from './App.jsx';
 import { getAsset, resolveAssetUrl } from './assets/assetRegistry.js';
 import './styles.css';
 
-const initialRoomAsset = getAsset('practice_room_001');
-if (initialRoomAsset.available) {
-  useGLTF.preload(resolveAssetUrl(initialRoomAsset));
-}
+[
+  'practice_room_001',
+  'recording_room_001',
+  'mix_room_001',
+  'mastering_room_001'
+].forEach((assetId)=>{
+  const asset=getAsset(assetId);
+  if(asset.available)useGLTF.preload(resolveAssetUrl(asset));
+});
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
